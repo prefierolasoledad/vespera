7ca4c3de016980889ff97a4992a77207988da5a7
prefierolasoledad <karanbhatt9900@gmail.com>
2024-10-02 20:30:43 +0530

    temp nav bar added

--- src/components/speakersPage.jsx
+++ src/components/speakersPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Speakers from './speakers/speakers'
+import Navbar from './navbar'
 
 function SpeakersPage() {
   return (
     <>
+      <Navbar/>
       <Speakers/>
     </>
   )
